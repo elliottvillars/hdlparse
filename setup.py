a8b2384cbf0c522eb83702f388c3226e50f6ac6a
Elliott Villars <elliottvillars@gmail.com>
2020-08-12 11:47:48 -0700

    Formatted code with yapf
    Signed-off-by: Elliott Villars <elliottvillars@gmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-
 from setuptools import setup
 
 # Use README.rst for the long description
 with open('README.rst') as fh:
     long_description = fh.read()
-    
+
 # Scan the script for the version string
 version_file = 'hdlparse/minilexer.py'
 version = None
@@ -16,35 +15,31 @@ with open(version_file) as fh:
         pass
 
 if version is None:
-    raise RuntimeError('Unable to find version string in file: {0}'.format(version_file))
-
+    raise RuntimeError(
+        'Unable to find version string in file: {0}'.format(version_file))
 
 setup(name='hdlparse',
-    version=version,
-    author='Kevin Thibedeau',
-    author_email='kevin.thibedeau@gmail.com',
-    url='http://kevinpt.github.io/hdlparse',
-    download_url='http://kevinpt.github.io/hdlparse',
-    description='HDL parser',
-    long_description=long_description,
-    platforms = ['Any'],
-    install_requires = [],
-    packages = ['hdlparse'],
-    py_modules = [],
-    include_package_data = True,
-
-    use_2to3 = True,
-    
-    keywords='HDL parser',
-    license='MIT',
-    classifiers=['Development Status :: 5 - Production/Stable',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'Topic :: Text Processing :: General',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
-        ]
-    )
-
+      version=version,
+      author='Kevin Thibedeau',
+      author_email='kevin.thibedeau@gmail.com',
+      url='http://kevinpt.github.io/hdlparse',
+      download_url='http://kevinpt.github.io/hdlparse',
+      description='HDL parser',
+      long_description=long_description,
+      platforms=['Any'],
+      install_requires=[],
+      packages=['hdlparse'],
+      py_modules=[],
+      include_package_data=True,
+      use_2to3=True,
+      keywords='HDL parser',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Topic :: Text Processing :: General', 'Natural Language :: English',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License'
+      ])
